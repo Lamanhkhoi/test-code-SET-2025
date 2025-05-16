91109bb55380128e09edc00ea885764437f743bc
khoi-3 <lamanhkhoi06@gmail.com>
2025-05-16 09:28:18 +0700

    few fixing

--- 4_kyu/soManyPermutations.js
+++ 4_kyu/soManyPermutations.js
@@ -1,3 +1,13 @@
 function permutations(string){
-    
+    let result = []
+    if string.length === 0{
+        return []
+    }
+    if string.length === 1 {
+        return [string]
+    }
+
+    for ( let i = 0 ; i < string.length ; i++ ){
+        
+    }
 }
